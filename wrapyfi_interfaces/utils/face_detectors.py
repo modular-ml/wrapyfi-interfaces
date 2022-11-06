edb443d3b8f77250612cdcb6f282c24ef005370a
abawi <fares.abawi@uni-hamburg.de>
2022-11-06 23:45:05 +0100

    Added face detector

--- wrapyfi_interfaces/utils/face_detectors.py
+++ wrapyfi_interfaces/utils/face_detectors.py
@@ -5,10 +5,10 @@ class DlibFaceDetection(object):
         import face_recognition
         self.__face_recognition__ = face_recognition
 
-    def detect_frames(self, video_frames_list, match_face=None, **kwargs):
+    def detect_bboxes(self, video_frames_list, match_face=None, **kwargs):
         # loading the features for tracking
         if match_face is not None:
-            p_image = self.__face_recognition__.load_image_file("face.jpg")
+            p_image = self.__face_recognition__.load_image_file(match_face)  # match_face is a path to an image
             p_encoding = self.__face_recognition__.face_encodings(p_image)[0]
 
         faces_locations = []
@@ -36,7 +36,7 @@ class MTCNNFaceDetection(object):
         self.__mtcnn__ = MTCNN(keep_all=True, device=device)
         self.__np__ = np
 
-    def detect_frames(self, video_frames_list, **kwargs):
+    def detect_bboxes(self, video_frames_list, **kwargs):
         faces_locations = []
         for f_idx in range(len(video_frames_list)):
             # detect faces
@@ -52,7 +52,7 @@ class MTCNNFaceDetection(object):
 
 
 class SFDFaceDetection:
-    def __init__(self, landmarks_type=1, device='cuda:0', flip_input=False, verbose=False):
+    def __init__(self, filter_threshold=0.5, device='cuda:0', verbose=False):
         import torch
         import torch.backends.cudnn as cudnn
         from face_alignment.detection.sfd import FaceDetector
@@ -60,17 +60,15 @@ class SFDFaceDetection:
         self.__torch__ = torch
         self.__np__ = np
         self.device = device
-        self.flip_input = flip_input
-        self.landmarks_type = landmarks_type
         self.verbose = verbose
 
         if 'cuda' in device:
             cudnn.benchmark = True
 
         # Get the face detector
-        self.face_detector = FaceDetector(device=device, verbose=verbose)
+        self.face_detector = FaceDetector(device=device, filter_threshold=filter_threshold, verbose=verbose)
 
-    def detect_frames(self, video_frames_list, **kwargs):
+    def detect_bboxes(self, video_frames_list, **kwargs):
         # images = self.__np__.asarray(video_frames_list)[..., ::-1]
         # images = self.__np__.squeeze(images, axis=1)
         # images = self.__torch__.FloatTensor(images)
@@ -90,3 +88,54 @@ class SFDFaceDetection:
                 boxes.append([y1, x2, y2, x1])
             face_locations.append(boxes)
         return face_locations
+
+
+class BlazeFaceDetection:
+    def __init__(self, min_score_threshold=0.5, min_suppression_threshold=0.5, device='cuda:0', verbose=False):
+        import torch
+        import torch.backends.cudnn as cudnn
+        from face_alignment.detection.blazeface import FaceDetector
+        import numpy as np
+        self.__torch__ = torch
+        self.__np__ = np
+        self.device = device
+        self.verbose = verbose
+
+        if 'cuda' in device:
+            cudnn.benchmark = True
+
+        # Get the face detector
+        self.face_detector = FaceDetector(device=device,
+                                          min_score_thresh=min_score_threshold,
+                                          min_suppression_threshold=min_suppression_threshold,
+                                          verbose=verbose)
+
+    def detect_bboxes(self, video_frames_list, **kwargs):
+        # images = self.__np__.asarray(video_frames_list)[..., ::-1]
+        # images = self.__np__.squeeze(images, axis=1)
+        # images = self.__torch__.FloatTensor(images)
+        images = self.__np__.moveaxis(self.__np__.stack(video_frames_list), -1, 1)
+        images =  self.__torch__.from_numpy(images).to(device=self.device)
+        detected_faces = self.face_detector.detect_from_batch(images)
+        face_locations = []
+
+        for i, d in enumerate(detected_faces):
+            if len(d) == 0:
+                face_locations.append([])
+                continue
+            boxes = []
+            for b in d:
+                b = self.__np__.clip(b, 0, None)
+                x1, y1, x2, y2 = map(int, b[:-1])
+                boxes.append([y1, x2, y2, x1])
+            face_locations.append(boxes)
+        return face_locations
+
+
+# TODO (fabawi): add face detector from https://github.com/elliottzheng/face-detection
+class RetinaFaceDetection:
+    def __init__(self):
+        pass
+
+    def detect_bboxes(self, video_frames_list, **kwargs):
+        pass
