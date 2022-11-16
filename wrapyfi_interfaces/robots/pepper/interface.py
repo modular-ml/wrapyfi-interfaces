15a27200eb00c7e48ac4c6259aefdd2a8902853b
Philipp Allgeuer <philipp.allgeuer@uni-hamburg.de>
2022-11-16 12:02:42 +0100

    WIP on Pepper interface

--- wrapyfi_interfaces/robots/pepper/interface.py
+++ wrapyfi_interfaces/robots/pepper/interface.py
@@ -1,7 +1,6 @@
 import os
 import time
 import argparse
-import logging
 from collections import deque
 
 import cv2
@@ -42,36 +41,28 @@ EMOTION_LOOKUP = {
 
 
 class Pepper(MiddlewareCommunicator):
+
     MWARE = PEPPER_DEFAULT_COMMUNICATOR
     CAP_PROP_FRAME_WIDTH = 640
     CAP_PROP_FRAME_HEIGHT = 480
+    LOOP_RATE = 30
     FACIAL_EXPRESSIONS_PORT = "/control_interface/facial_expressions"
-    # constants
     FACIAL_EXPRESSIONS_QUEUE_SIZE = 50
     FACIAL_EXPRESSION_SMOOTHING_WINDOW = 6
 
-    def __init__(self, simulation=False, headless=False, get_cam_feed=True,
+    def __init__(self, headless=False, get_cam_feed=True,
                  img_width=CAP_PROP_FRAME_WIDTH, img_height=CAP_PROP_FRAME_HEIGHT,
                  control_expressions=False,
                  set_facial_expressions=True, facial_expressions_port=FACIAL_EXPRESSIONS_PORT,
                  mware=MWARE):
+
         self.__name__ = "Pepper"
-        MiddlewareCommunicator.__init__(self)
+        super().__init__()
 
         self.MWARE = mware
         self.FACIAL_EXPRESSIONS_PORT = facial_expressions_port
-
         self.headless = headless
-
-        if simulation:
-            # TODO: PLACE CAM PROPS IN DICT
-            self.cam_props = {"cam_world_port": "/Sim/cam",
-                              "cam_left_port": "/Sim/cam/left",
-                              "cam_right_port": "/Sim/cam/right"}
-        else:
-            self.cam_props = {"cam_world_port": "/Robot/cam/left",
-                              "cam_left_port": "/Robot/cam/left",
-                              "cam_right_port": "/Robot/cam/right"}
+        self.cam_props = {"cam_front_port": "/pepper/camera/front/camera/image_raw"}
 
         if img_width is not None:
             self.img_width = img_width
@@ -85,7 +76,6 @@ class Pepper(MiddlewareCommunicator):
 
         if control_expressions:
             # control emotional expressions
-            # TODO: DECLARE VARS FOR CONTROLLING EXPRESSIONS
             self.last_expression = ["", ""]  # (emotion part on the robot's face , emotional expression category)
             self.expressions_queue = deque(maxlen=self.FACIAL_EXPRESSIONS_QUEUE_SIZE)
         else:
@@ -106,63 +96,51 @@ class Pepper(MiddlewareCommunicator):
         Pepper.update_facial_expressions.__defaults__ = ("LIGHTS", None, self.MWARE)
         Pepper.receive_images.__defaults__ = (self.CAP_PROP_FRAME_WIDTH, self.CAP_PROP_FRAME_HEIGHT, True)
 
-    @MiddlewareCommunicator.register("NativeObject", "$_mware",
-                                     "Pepper", "$facial_expressions_port",
-                                     should_wait=False)
-    def acquire_facial_expressions(self, facial_expressions_port=FACIAL_EXPRESSIONS_PORT, cv2_key=None,
-                                   _mware=MWARE):
+    @MiddlewareCommunicator.register("NativeObject", "$_mware", "Pepper", "$facial_expressions_port", should_wait=False)
+    def acquire_facial_expressions(self, facial_expressions_port=FACIAL_EXPRESSIONS_PORT, cv2_key=None, _mware=MWARE):
         emotion = None
         if cv2_key is None:
-            logging.error("controlling expressions in headless mode not yet supported")
+            print("Error: Controlling expressions in headless mode not yet supported")
             return None,
         else:
             if cv2_key == 27:  # Esc key to exit
                 exit(0)
-            elif cv2_key == -1:  # normally -1 returned,so don"t print it
+            elif cv2_key == -1:  # normally -1 returned,so don't print it
                 pass
-            # TODO: REPLACE emotion WITH STRING UNDERSTOOD BY PEPPER
             elif cv2_key == 49:  # 1 key: sad emotion
                 emotion = "sad"
-                logging.info("expressing sadness")
+                print("Info: Expressing sadness")
             elif cv2_key == 50:  # 2 key: angry emotion
                 emotion = "ang"
-                logging.info("expressing anger")
+                print("Info: Expressing anger")
             elif cv2_key == 51:  # 3 key: happy emotion
                 emotion = "hap"
-                logging.info("expressing happiness")
+                print("Info: Expressing happiness")
             elif cv2_key == 52:  # 4 key: neutral emotion
                 emotion = "neu"
-                logging.info("expressing neutrality")
+                print("Info: Expressing neutrality")
             elif cv2_key == 53:  # 5 key: surprise emotion
                 emotion = "sur"
-                logging.info("expressing surprise")
+                print("Info: Expressing surprise")
             elif cv2_key == 54:  # 6 key: shy emotion
                 emotion = "shy"
-                logging.info("expressing shyness")
+                print("Info: Expressing shyness")
             elif cv2_key == 55:  # 7 key: evil emotion
                 emotion = "evi"
-                logging.info("expressing evilness")
+                print("Info: Expressing evilness")
             elif cv2_key == 56:  # 8 key: cunning emotion
                 emotion = "cun"
-                logging.info("expressing cunningness")
+                print("Info: Expressing cunningness")
             else:
-                logging.info(cv2_key)  # else print its value
+                print(f"Info: Other key {cv2_key}")  # else print its value
                 return None,
             return {"topic": facial_expressions_port.split("/")[-1],
                     "timestamp": time.time(),
                     "emotion_category": emotion},
 
-    @MiddlewareCommunicator.register("NativeObject", "$_mware",
-                                     "Pepper", "/pepper_controller/logs/facial_expressions",
-                                     should_wait=False)
+    @MiddlewareCommunicator.register("NativeObject", "$_mware", "Pepper", "/pepper_controller/logs/facial_expressions", should_wait=False)
     def update_facial_expressions(self, expression, part="LIGHTS", smoothing="mode", _mware=MWARE):
-        """
-        Control facial expressions of the Pepper
-        :param expression: Expression abbreviation
-        :param part: Abbreviation describing parts to control
-        :param smoothing: Name of smoothing filter to avoid abrupt changes in emotional expressions
-        :return: None
-        """
+
         if expression is None:
             return None,
         if isinstance(expression, (list, tuple)):
@@ -171,8 +149,7 @@ class Pepper(MiddlewareCommunicator):
 
         if smoothing == "mode":
             self.expressions_queue.append(expression)
-            transmitted_expression = mode_smoothing_filter(list(self.expressions_queue),
-                                                           window_length=self.FACIAL_EXPRESSION_SMOOTHING_WINDOW)
+            transmitted_expression = mode_smoothing_filter(list(self.expressions_queue), window_length=self.FACIAL_EXPRESSION_SMOOTHING_WINDOW)
         else:
             transmitted_expression = expression
 
@@ -192,53 +169,36 @@ class Pepper(MiddlewareCommunicator):
                 "timestamp": time.time(),
                 "command": f"emotion set to {part} {expression} with smoothing={smoothing}"},
 
-    @MiddlewareCommunicator.register("Image", "ros", "Pepper", "$cam_world_port",
-                                     width="$img_width", height="$img_height", _rgb="$rgb")
-    @MiddlewareCommunicator.register("Image", "ros", "Pepper", "$cam_left_port",
-                                     width="$img_width", height="$img_height", _rgb="$rgb")
-    @MiddlewareCommunicator.register("Image", "ros", "Pepper", "$cam_right_port",
-                                     width="$img_width", height="$img_height", _rgb="$rgb")
-    def receive_images(self, cam_world_port, cam_left_port, cam_right_port,
-                       img_width=CAP_PROP_FRAME_WIDTH, img_height=CAP_PROP_FRAME_HEIGHT, _rgb=True):
-        external_cam, left_cam, right_cam = None, None, None
-        return external_cam, left_cam, right_cam
-
-    def getPeriod(self):
-        return 0.03
+    @MiddlewareCommunicator.register("Image", "ros", "Pepper", "$cam_front_port", width="$img_width", height="$img_height", rgb="$_rgb")
+    def receive_images(self, cam_front_port, img_width=CAP_PROP_FRAME_WIDTH, img_height=CAP_PROP_FRAME_HEIGHT, _rgb=True):
+        return None,
 
     def updateModule(self):
-        # print(self.getPeriod())
-        external_cam, left_cam, right_cam = self.receive_images(**self.cam_props)
-        if external_cam is None:
-            external_cam = np.zeros((self.img_height, self.img_width, 1), dtype="uint8")
-            left_cam = np.zeros((self.img_height, self.img_width, 1), dtype="uint8")
-            right_cam = np.zeros((self.img_height, self.img_width, 1), dtype="uint8")
-        else:
-            external_cam = cv2.cvtColor(external_cam, cv2.COLOR_BGR2RGB)
-            left_cam = cv2.cvtColor(left_cam, cv2.COLOR_BGR2RGB)
-            right_cam = cv2.cvtColor(right_cam, cv2.COLOR_BGR2RGB)
+
+        front_cam, = self.receive_images(**self.cam_props)
+        if front_cam is None:
+            front_cam = np.zeros((self.img_height, self.img_width, 1), dtype="uint8")
+
         if not self.headless:
-            cv2.imshow("PepperCam", np.concatenate((left_cam, external_cam, right_cam), axis=1))
-            k = cv2.waitKey(30)
+            cv2.imshow("PepperCam", front_cam)
+            k = cv2.pollKey()
         else:
             k = None
 
-        switch_emotion, = self.acquire_facial_expressions(facial_expressions_port=self.FACIAL_EXPRESSIONS_PORT,
-                                                          cv2_key=k, _mware=self.MWARE)
+        switch_emotion, = self.acquire_facial_expressions(facial_expressions_port=self.FACIAL_EXPRESSIONS_PORT, cv2_key=k, _mware=self.MWARE)
         if switch_emotion is not None and isinstance(switch_emotion, dict):
-            self.update_facial_expressions(switch_emotion.get("emotion_category", None),
-                                           part=switch_emotion.get("part", "LIGHTS"), _mware=self.MWARE)
+            self.update_facial_expressions(switch_emotion.get("emotion_category", None), part=switch_emotion.get("part", "LIGHTS"), _mware=self.MWARE)
 
         return True
 
     def runModule(self):
+        period = 1 / self.LOOP_RATE
         while True:
             self.updateModule()
-            time.sleep(self.getPeriod())
+            time.sleep(period)
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--simulation", action="store_true", help="Run in simulation")
     parser.add_argument("--headless", action="store_true", help="Disable CV2 GUI")
     parser.add_argument("--get_cam_feed", action="store_true", help="Get the camera feeds from the robot")
     parser.add_argument("--control_expressions", action="store_true", help="Control the facial expressions")
@@ -258,7 +218,6 @@ def parse_args():
 
 if __name__ == "__main__":
     args = parse_args()
-    assert not (args.headless and (args.set_facial_expressions or args.set_head_eye_coordinates)), \
-        "setters require a CV2 window for capturing keystrokes. Disable --set-... for running in headless mode"
+    assert not (args.headless and (args.set_facial_expressions or args.set_head_eye_coordinates)), "Setters require a CV2 window for capturing keystrokes. Disable --set_* for running in headless mode"
     controller = Pepper(**vars(args))
     controller.runModule()
