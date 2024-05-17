54ec3730149f4f3c8849279c23a18f35b29d89c9
Fares Abawi <fares.abawi@uni-hamburg.de>
2024-05-17 14:50:21 +0200

    Update main.c

--- wrapyfi_interfaces/sensors/waveshare_imu/pico/c/main.c
+++ wrapyfi_interfaces/sensors/waveshare_imu/pico/c/main.c
@@ -14,6 +14,8 @@
 
 	You should have received a copy of the GNU General Public License
 	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+	Modified by Fares Abawi (f.abawi@outlook.com)
 */
 #include "icm20948.h"
 #include "lps22hb.h"
